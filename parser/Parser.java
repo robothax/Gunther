1a5bfc26008968118522891cadc1eb7f2a388e85
Jeffrey <robothax@gmail.com>
2012-11-19 15:22:19 -0500

    Bare skeleton code for parser
    parser will consist of static methods
    no need for an object really

---
+++ parser/Parser.java
@@ -0,0 +1,39 @@
+package parser;
+
+import data_structures.*;
+
+public class Parser {
+	
+	public static final String IMPLICATION_SYMBOL = "=>";
+	public static final String CONJUNCTION_SYMBOL = "/\\";
+	public static final String DISJUNCTION_SYMBOL = "\\/";
+	public static final String NEGATION_SYMBOL = "~";
+	public static final String EQUIVALENCE_SYMBOL = "<=>";
+	
+	public static Sequent parse(String input) {
+		Sequent init = new Sequent();
+		
+		Formula[] literals = new Formula[26]; //dynamically create this later
+		
+		int len = input.length();
+		int i = 0;
+		
+		/* Pass 1: populate a list of active literals */
+		while(i < len) {
+			char buf = input.charAt(i);
+			if(Character.isLetter(buf)) {
+				if (literals[buf-'A'] != null) {
+					literals[buf-'A'] = new Formula(buf);
+				}
+			}
+		}
+		
+		i = 0;
+		/* Pass 2: generate the initial formula structure */
+		while(i < len) {
+			
+		}
+		
+		return init;
+	}
+}
